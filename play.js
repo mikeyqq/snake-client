13ef737e920b900ccaf6ebcd9afc97d1a645b5d6
Anchen Huang <acmhuang@gmail.com>
2019-07-03 19:18:23 +0000

    created handleUserInput and setu[Input

--- play.js
+++ play.js
@@ -1,3 +1,34 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+
+const handleUserInput = function(key){
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    console.log('Move Up')
+  }
+  if (key === 's') {
+    console.log('Move Down')
+  }
+  if (key === 'a') {
+    console.log('Move Left')
+  }
+  if (key === 'd') {
+    console.log('Move Right')
+  }
+  };
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  process.stdin.on('data', handleUserInput)
+  return stdin;
+}
+
+setupInput();
